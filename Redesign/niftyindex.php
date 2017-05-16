579761458c992679adf473cf46c051cc8ef96846
leagreen <galeasha@gmail.com>
2017-05-15 23:40:00 -0700

    Add files via upload

---
+++ Redesign/niftyindex.php
@@ -0,0 +1,32 @@
+<?php include 'header.php';?>
+        
+        <main>        
+            <section class="Featured">
+                <img src="images/nifty-tours-logo.png" alt="Nifty Logo">
+                <h1>Headline</h1>
+                <h2>SubHeader</h2>
+
+                <p>Paragraph Text Paragraph Text <a href='#'>anchor link</a> Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text</p>
+            </section>
+            
+            <div class="toplowerbar"></div>
+            <section class="RelatedLinks FirstLink col-md-4">
+                <h2>Related One - Header Two</h2>
+                <h3>Header Three</h3>
+                <p>Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text </p>
+            </section>
+                  
+            <section class="RelatedLinks SecondLink col-md-4">
+                <h2>Related Article Two</h2>
+                <h3>Header Three</h3>
+                <p>Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text </p>
+            </section>
+                
+            <section class="RelatedLinks ThirdLink col-md-4">
+                <h2>Related Article Three</h2>
+                <h3>Header Three</h3>
+                <p>Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text HELLO Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text Paragraph Text</p>
+            </section>
+        </main>
+        
+<?php include 'footer.php';?>
